4c73093200a028f145ecebc97ab398fbc39cefa8
Ivan Kukutsov <ivankukucov@gmail.com>
2023-10-24 00:18:32 +0300

    Test

---
+++ Health-prescription-software-API/Controllers/ValuesController4.cs
@@ -0,0 +1,9 @@
+﻿namespace Health_prescription_software_API.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController4 : ControllerBase
+{
+}
