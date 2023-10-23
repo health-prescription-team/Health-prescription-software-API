84733eb763a7ac96a6fb5f1926982762038813ab
Nikola Madzharov <nmadzharov1912@gmail.com>
2023-10-24 00:15:03 +0300

    test

---
+++ Health-prescription-software-API/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Health_prescription_software_API.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
