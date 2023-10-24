ef7edf82f975e3dc5e8a4f33bead50acdc5ac620
Ivan Kukutsov <ivankukucov@gmail.com>
2023-10-24 21:59:46 +0300

    Added details endpoint to MedicineController

---
+++ Health-prescription-software-API/Controllers/MedicineController.cs
@@ -0,0 +1,31 @@
+﻿namespace Health_prescription_software_API.Controllers;
+
+using Health_prescription_software_API.Contracts;
+using Health_prescription_software_API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MedicineController : ControllerBase
+{
+    private IMedicineService _medicineService;
+
+    public MedicineController(MedicineService medicineService)
+    {
+        _medicineService = medicineService;
+    }
+
+    [HttpGet("api/[controller]/id")]
+    public async Task<IActionResult> Details(int id)
+    {
+        var medicine = await _medicineService.GetById(id);
+
+        if (medicine == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(medicine);
+    }
+}
