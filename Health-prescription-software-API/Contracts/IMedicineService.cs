3db55e02bd8267b9659a3887310217261769f75c
Ivan Kukutsov <ivankukucov@gmail.com>
2023-10-24 21:59:01 +0300

    Added GetById to MedicineService

--- Health-prescription-software-API/Contracts/IMedicineService.cs
+++ Health-prescription-software-API/Contracts/IMedicineService.cs
@@ -1,6 +1,9 @@
 ﻿namespace Health_prescription_software_API.Contracts
 {
+    using Health_prescription_software_API.Data.Entities;
+
     public interface IMedicineService
     {
+        Task<Medicine?> GetById(int id);
     }
 }
--- Health-prescription-software-API/Services/MedicineService.cs
+++ Health-prescription-software-API/Services/MedicineService.cs
@@ -1,5 +1,6 @@
 ﻿using Health_prescription_software_API.Contracts;
 using Health_prescription_software_API.Data;
+using Health_prescription_software_API.Data.Entities;
 
 namespace Health_prescription_software_API.Services
 {
@@ -10,6 +11,11 @@ namespace Health_prescription_software_API.Services
         public MedicineService(HealthPrescrtionDbContext context)
         {
             _context = context;
+        }
+
+        public async Task<Medicine?> GetById(int id)
+        {
+            return await _context.Medicines.FindAsync(id);
         }
     }
 }
