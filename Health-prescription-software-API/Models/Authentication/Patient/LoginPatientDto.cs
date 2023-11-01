7a60773a60ae7214b9c31d4aa28c5baca0c8507e
monich <simnaosoyska@gmail.com>
2023-11-01 23:55:05 +0200

    egn changed to string

--- Health-prescription-software-API/Models/Authentication/Patient/LoginPatientDto.cs
+++ Health-prescription-software-API/Models/Authentication/Patient/LoginPatientDto.cs
@@ -5,7 +5,7 @@ namespace Health_prescription_software_API.Models.Authentication.Patient
     public class LoginPatientDto
     {
         [Required]
-        public int Egn { get; set; }
+        public string Egn { get; set; }
 
         [Required]
         public string Password { get; set; } = null!;
