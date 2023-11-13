5447f87104a2fa6da326eda9124f95ece3522e3d
Ivan Kukutsov <ivankukucov@gmail.com>
2023-11-14 00:25:30 +0200

    Validation servicees refactored to use the ModelError collection.

--- Health-prescription-software-API/Services/ValidationServices/ValidationAuthentication.cs
+++ Health-prescription-software-API/Services/ValidationServices/ValidationAuthentication.cs
@@ -2,36 +2,42 @@
 {
     using Health_prescription_software_API.Contracts.Validations;
     using Health_prescription_software_API.Data;
-	using Health_prescription_software_API.Models.Authentication.Pharmacy;
-	using Microsoft.EntityFrameworkCore;
-	using System.Threading.Tasks;
+    using Health_prescription_software_API.Models.Authentication.Pharmacy;
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
 
     public class ValidationAuthentication : IValidationAuthentication
-	{
-		private readonly HealthPrescriptionDbContext dbContext;
+    {
+        private readonly HealthPrescriptionDbContext dbContext;
 
-		public ValidationAuthentication(HealthPrescriptionDbContext dbContext)
-		{
-			this.dbContext = dbContext;
-		}
+        public ValidationAuthentication(HealthPrescriptionDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.ModelErrors = new HashSet<ModelError>();
+        }
 
-		public string? ErrorMessage { get; set; }
+        public ICollection<ModelError> ModelErrors { get; set; }
 
-		public string? ErrorPropName { get; set; }
+        public async Task<bool> IsPharmacyRegisterValid(RegisterPharmacyDto registerModel)
+        {
+            bool isEmailPresent = await dbContext.Users
+                .AnyAsync(u => u.Email == registerModel.Email);
+            if (isEmailPresent)
+            {
+                var error = new ModelError
+                {
+                    ErrorPropName = nameof(registerModel.Email),
+                    ErrorMessage = "User with the same email already exists."
+                };
 
-		public async Task<bool> IsPharmacyRegisterValid(RegisterPharmacyDto registerModel)
-		{
-			bool isEmailPresent = await dbContext.Users
-				.AnyAsync(u => u.Email == registerModel.Email);
-			if (isEmailPresent)
-			{
-				ErrorPropName = nameof(registerModel.Email);
-				ErrorMessage = "User with the same email already exists.";
-				return false;
-			}
-			// todo: more checking if needed
+                ModelErrors.Add(error);
 
-			return true;
-		}
-	}
+                return false;
+            }
+            // todo: more checking if needed
+
+            return true;
+        }
+    }
 }
