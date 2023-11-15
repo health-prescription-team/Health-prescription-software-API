0639743349fa8962df2caec718eedf76b1dbe931
Ivan Kukutsov <ivankukucov@gmail.com>
2023-11-15 21:29:40 +0200

    Services test project setup.

---
+++ Health-prescription-software-API.Services.Tests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using NUnit.Framework;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Query;
+global using Moq;
+global using Health_prescription_software_API.Services.Tests.Utilities;
---
+++ Health-prescription-software-API.Services.Tests/Utilities/TestAsyncQueryHelpers.cs
@@ -0,0 +1,101 @@
+﻿namespace Health_prescription_software_API.Services.Tests.Utilities
+{
+    using System.Linq.Expressions;
+
+    public class TestAsyncQueryProvider<TEntity> : IAsyncQueryProvider
+    {
+        private readonly IQueryProvider inner;
+
+        internal TestAsyncQueryProvider(IQueryProvider inner)
+        {
+            this.inner = inner;
+        }
+
+        public IQueryable CreateQuery(Expression expression)
+        {
+            return new TestAsyncQueryable<TEntity>(expression);
+        }
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            return new TestAsyncQueryable<TElement>(expression);
+        }
+
+        public object Execute(Expression expression)
+        {
+            return inner.Execute(expression)!;
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            return inner.Execute<TResult>(expression);
+        }
+
+        public IAsyncEnumerable<TResult> ExecuteAsync<TResult>(Expression expression)
+        {
+            return new TestAsyncEnumerable<TResult>(expression);
+        }
+
+        TResult IAsyncQueryProvider.ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
+        {
+            return inner.Execute<TResult>(expression);
+        }
+    }
+
+    public class TestAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+    {
+        public TestAsyncEnumerable(IEnumerable<T> enumerable)
+            : base(enumerable)
+        { }
+
+        public TestAsyncEnumerable(Expression expression)
+            : base(expression)
+        { }
+
+        public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+        {
+            return new TestAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+        }
+    }
+
+    public class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> inner;
+
+        public TestAsyncEnumerator(IEnumerator<T> inner)
+        {
+            this.inner = inner;
+        }
+
+        public T Current => inner.Current;
+
+        public ValueTask<bool> MoveNextAsync()
+        {
+            return new ValueTask<bool>(inner.MoveNext());
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            inner.Dispose();
+            return default;
+        }
+    }
+
+    public class TestAsyncQueryable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+    {
+        public TestAsyncQueryable(IEnumerable<T> enumerable)
+            : base(enumerable)
+        { }
+
+        public TestAsyncQueryable(Expression expression)
+            : base(expression)
+        { }
+
+        public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+        {
+            return new TestAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+        }
+
+        IQueryProvider IQueryable.Provider => new TestAsyncQueryProvider<T>(this);
+    }
+}
