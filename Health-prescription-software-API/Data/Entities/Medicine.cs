3b23e97ca26be2854ba864d2fc4e619707695528
TodorZarkov <t.p.zarkov@gmail.com>
2023-11-19 14:47:05 +0200

    changed Price in Medicine Entity AveragePrice

--- Health-prescription-software-API/Data/Entities/Medicine.cs
+++ Health-prescription-software-API/Data/Entities/Medicine.cs
@@ -22,7 +22,7 @@ namespace Health_prescription_software_API.Data.Entities
         public byte[]? MedicineImageBytes { get; set; }
 
         [Required]
-        public decimal AveragePrice { get; set; } //average
+        public decimal AveragePrice { get; set; }
 
         [Required] 
         public string MedicineCompany { get; set; } = null!;
