8bef49c44ef683850fcd635d33af00ad1afd7012
Ivan Kukutsov <ivankukucov@gmail.com>
2023-11-24 00:29:36 +0200

    Added PrescriptionDetailsDto

---
+++ Health-prescription-software-API/Models/Prescription/PrescriptionDetailsDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Health_prescription_software_API.Models.Prescription
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class PrescriptionDetailsDto
+    {
+        [Required]
+        public int MedicineId { get; set; }
+
+        [Required]
+        public int EveningDose { get; set; }
+
+        [Required]
+        public int LunchTimeDose { get; set; }
+
+        [Required]
+        public int MorningDose { get; set; }
+
+        public string? Notes { get; set; }
+    }
+}
