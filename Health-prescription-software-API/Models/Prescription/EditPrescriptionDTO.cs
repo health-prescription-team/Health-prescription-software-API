ab40f768a77406f7eae43d6600a02aadfebb9278
Ivan Kukutsov <ivankukucov@gmail.com>
2023-12-19 00:30:16 +0200

    Edit prescription and prescriptiondetails dtos

---
+++ Health-prescription-software-API/Models/Prescription/EditPrescriptionDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace Health_prescription_software_API.Models.Prescription
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using static Common.EntityValidationConstants.User;
+
+    public class EditPrescriptionDTO
+    {
+        public EditPrescriptionDTO()
+        {
+            PrescriptionDetails = new HashSet<EditPrescriptionDetailsDTO>();
+        }
+
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        [RegularExpression(EgnRegexPattern, ErrorMessage = InvalidEgnErrorMessage)]
+        public string PatientEgn { get; set; } = null!;
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public string Diagnosis { get; set; } = null!;
+
+        public DateTime? ExpiresAt { get; set; }
+
+        public ICollection<EditPrescriptionDetailsDTO> PrescriptionDetails { get; set; }
+    }
+}
---
+++ Health-prescription-software-API/Models/Prescription/EditPrescriptionDetailsDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace Health_prescription_software_API.Models.Prescription
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class EditPrescriptionDetailsDTO
+    {
+        [Required]
+        public Guid MedicineId { get; set; }
+
+        [Required]
+        public int EveningDose { get; set; }
+
+        [Required]
+        public int LunchTimeDose { get; set; }
+
+        [Required]
+        public int MorningDose { get; set; }
+
+        [Required]
+        public string MeasurementUnit { get; set; } = null!;
+
+        public string? Notes { get; set; }
+    }
+}
