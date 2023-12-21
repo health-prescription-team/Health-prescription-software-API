0e5c303ed932e96d283ceb34de50330e17a53dd1
Ivan Kukutsov <ivankukucov@gmail.com>
2023-12-21 14:36:09 +0200

    Tests Prescription seed

---
+++ Health-prescription-software-API.Services.Tests/Seeding/PrescriptionSeed.cs
@@ -0,0 +1,29 @@
+﻿namespace Health_prescription_software_API.Services.Tests.Seeding
+{
+    using Data.Entities;
+
+    public static class PrescriptionSeed
+    {
+        public static Prescription[] GeneratePrescriptions()
+        {
+            return
+            [
+                new Prescription()
+                {
+                   
+                }
+            ];
+        }
+
+        public static PrescriptionDetails[] GeneratePrescriptionDetails()
+        {
+            return
+            [
+                new PrescriptionDetails()
+                {
+
+                }
+            ];
+        }
+    }
+}
