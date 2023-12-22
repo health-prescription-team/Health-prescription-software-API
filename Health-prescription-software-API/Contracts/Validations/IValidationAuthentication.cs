55e4d79744ce35f77008d2835eaf53e7e180d6c5
Nikola Madzharov <nmadzharov1912@gmail.com>
2023-12-23 01:21:48 +0200

    Added register validations for the GP user

--- Health-prescription-software-API/Contracts/Validations/IValidationAuthentication.cs
+++ Health-prescription-software-API/Contracts/Validations/IValidationAuthentication.cs
@@ -3,8 +3,9 @@
     using Health_prescription_software_API.Models.Authentication.Pharmacist;
     using Health_prescription_software_API.Models.Authentication.Pharmacy;
     using Health_prescription_software_API.Models.Authentication.Patient;
+    using Health_prescription_software_API.Models.Authentication.GP;
 
-	public interface IValidationAuthentication : IValidationErrorMessage
+    public interface IValidationAuthentication : IValidationErrorMessage
     {
         Task<bool> IsPharmacyLoginValid(LoginPharmacyDto registerModel);
         Task<bool> IsPharmacyRegisterValid(RegisterPharmacyDto registerModel);
@@ -16,5 +17,7 @@
         Task<bool> IsPatientRegisterValid(PatientDto registerModel);
 
         Task<bool> IsPatientLoginValid(LoginPatientDto loginModel);
+
+        Task<bool> IsGpRegisterValid(RegisterGpDto registerModel);
     }
 }
