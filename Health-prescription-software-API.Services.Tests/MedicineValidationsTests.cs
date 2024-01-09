bbbb7ce7b533140f1e6e1ce3ccee56aa7e2ecb59
Ivan Kukutsov <ivankukucov@gmail.com>
2024-01-09 21:59:43 +0200

    IsPharmacyMedicineOwner tests

--- Health-prescription-software-API.Services.Tests/MedicineValidationsTests.cs
+++ Health-prescription-software-API.Services.Tests/MedicineValidationsTests.cs
@@ -75,5 +75,43 @@
 
             Assert.That(actualResult, Is.False);
         }
+
+        [Test]
+        public async Task IsPharmacyMedicineOwnerReturnsTrueWithActualOwner()
+        {
+            // Arrange
+
+            var validationService = new ValidationMedicine(dbContext);
+
+            var medicineId = Guid.Parse("b7540da6-da0f-40b0-bd8c-259e42e3af8d");
+            var pharmacyId = "31bcded4-bbe9-45a3-b235-2d24766cfdf3";
+
+            // Act
+
+            bool actualResult = await validationService.IsPharmacyMedicineOwner(pharmacyId, medicineId);
+
+            // Assert
+
+            Assert.That(actualResult, Is.True);
+        }
+
+        [Test]
+        public async Task IsPharmacyMedicineOwnerReturnsTrueWithNonOwnerId()
+        {
+            // Arrange
+
+            var validationService = new ValidationMedicine(dbContext);
+
+            var medicineId = Guid.Parse("b7540da6-da0f-40b0-bd8c-259e42e3af8d");
+            var pharmacyId = "bb02a027-408e-49d7-86d3-8e7b5471feba";
+
+            // Act
+
+            bool actualResult = await validationService.IsPharmacyMedicineOwner(pharmacyId, medicineId);
+
+            // Assert
+
+            Assert.That(actualResult, Is.False);
+        }
     }
 }
