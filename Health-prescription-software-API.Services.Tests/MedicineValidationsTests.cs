45740abd40cc0ae5ddb6075e8785533a7d76ae99
Ivan Kukutsov <ivankukucov@gmail.com>
2024-01-09 21:55:40 +0200

    IsMedicineValid tests

---
+++ Health-prescription-software-API.Services.Tests/MedicineValidationsTests.cs
@@ -0,0 +1,79 @@
+﻿namespace Health_prescription_software_API.Services.Tests
+{
+    using Data;
+    using Models.Medicine;
+
+    using Services.ValidationServices;
+
+    using static Seeding.UserSeed;
+    using static Seeding.PrescriptionSeed;
+    using static Seeding.MedicineSeed;
+
+    public class MedicineValidationsTests
+    {
+        private HealthPrescriptionDbContext dbContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            // In memory database setup
+
+            var options = new DbContextOptionsBuilder<HealthPrescriptionDbContext>()
+            .UseInMemoryDatabase(databaseName: "InMemoryHealthDB")
+                .Options;
+
+            dbContext = new HealthPrescriptionDbContext(options);
+
+            // Seed database
+
+            dbContext.AddRange(GenerateUsers());
+            dbContext.AddRange(GeneratePrescriptions());
+            dbContext.AddRange(GeneratePrescriptionDetails());
+            dbContext.AddRange(GenerateMedicine());
+
+            dbContext.SaveChanges();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            dbContext.Database.EnsureDeleted();
+        }
+
+        [Test]
+        public async Task IsMedicineValidWithExistingIdReturnsTrue()
+        {
+            // Arrange
+
+            var validationService = new ValidationMedicine(dbContext);
+
+            var medicineId = Guid.Parse("b7540da6-da0f-40b0-bd8c-259e42e3af8d");
+
+            // Act
+
+            bool actualResult = await validationService.IsMedicineValid(medicineId);
+
+            // Assert
+
+            Assert.That(actualResult, Is.True);
+        }
+
+        [Test]
+        public async Task IsMedicineValidWithNonExistingIdReturnsTrue()
+        {
+            // Arrange
+
+            var validationService = new ValidationMedicine(dbContext);
+
+            var medicineId = Guid.Parse("b7540da6-da0f-40b0-bd8c-259e42e3af8a");
+
+            // Act
+
+            bool actualResult = await validationService.IsMedicineValid(medicineId);
+
+            // Assert
+
+            Assert.That(actualResult, Is.False);
+        }
+    }
+}
