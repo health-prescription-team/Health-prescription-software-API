6bfbed637c2dec14013448203a87b0e28837eec0
Ivan Kukutsov <ivankukucov@gmail.com>
2024-01-09 15:09:43 +0200

    AddingMedicine and GetById tests

---
+++ Health-prescription-software-API.Services.Tests/MedicineServiceTests.cs
@@ -0,0 +1,108 @@
+﻿namespace Health_prescription_software_API.Services.Tests
+{
+    using Microsoft.AspNetCore.Http;
+
+    using Data;
+    using Models.Medicine;
+
+    using static Seeding.UserSeed;
+    using static Seeding.PrescriptionSeed;
+    using static Seeding.MedicineSeed;
+
+    public class MedicineServiceTests
+    {
+        private HealthPrescriptionDbContext dbContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            // In memory database setup
+
+            var options = new DbContextOptionsBuilder<HealthPrescriptionDbContext>()
+                .UseInMemoryDatabase(databaseName: "InMemoryHealthDB")
+                .Options;
+
+            dbContext = new HealthPrescriptionDbContext(options);
+
+            // Seed database
+
+            dbContext.AddRange(GenerateUsers());
+            dbContext.AddRange(GeneratePrescriptions());
+            dbContext.AddRange(GeneratePrescriptionDetails());
+            dbContext.AddRange(GenerateMedicine());
+
+            dbContext.SaveChanges();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            dbContext.Database.EnsureDeleted();
+        }
+
+        [Test]
+        public async Task AddingMedicineReturnsId()
+        {
+            // Arrange
+
+            var medicineService = new MedicineService(dbContext);
+
+            var creatorId = "31bcded4-bbe9-45a3-b235-2d24766cfdf3";
+
+            AddMedicineDTO addModel = new()
+            {
+                Name = "Лекарство",
+                MedicineCompany = "Производител",
+                MedicineImage = new FormFile(new MemoryStream(), 0, 321654, "test", "test.png"),
+                MedicineDetails = "Лекарство за болни.",
+                Ingredients = "Химикали 10гр., Още химикали 20гр.",
+                Price = 22.22m
+            };
+
+            var expectedMedicineCount = await dbContext.Medicines.CountAsync() + 1;
+
+            // Act
+
+            var addedMedicineId = await medicineService.Add(addModel, creatorId);
+
+            // Assert
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(addedMedicineId, Is.Not.EqualTo(Guid.Empty));
+                Assert.That(dbContext.Medicines.Count(), Is.EqualTo(expectedMedicineCount));
+            });
+        }
+
+        [Test]
+        public async Task GetByIdReturnsMedicineDetailsDTO()
+        {
+            // Arrange
+
+            var medicineService = new MedicineService(dbContext);
+
+            var medicineId = Guid.Parse("b7540da6-da0f-40b0-bd8c-259e42e3af8d");
+
+            var expectedName = "Аспирин";
+            var expectedCompany = "Fake Pharma";
+            var expectedDetails = "Аспирин 50 мг.";
+            var expectedPrice = 12.33m;
+            var expectedIngredients = "Кора от дърво?";
+
+            // Act
+
+            MedicineDetailsDTO medicineDetailsDto = await medicineService.GetById(medicineId);
+
+            // Assert
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(medicineDetailsDto.Name, Is.EqualTo(expectedName));
+                Assert.That(medicineDetailsDto.MedicineCompany, Is.EqualTo(expectedCompany));
+                Assert.That(medicineDetailsDto.MedicineDetails, Is.EqualTo(expectedDetails));
+                Assert.That(medicineDetailsDto.Price, Is.EqualTo(expectedPrice));
+                Assert.That(medicineDetailsDto.Ingredients, Is.EqualTo(expectedIngredients));
+            });
+        }
+    }
+}
