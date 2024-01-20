b47473be4947393ac46f79cce8bbe38daef2e0ee
Ivan Kukutsov <ivankukucov@gmail.com>
2024-01-20 22:20:44 +0200

    Added chat message entity some atributes.

--- Health-prescription-software-API/Data/Entities/Chat/ChatMessage.cs
+++ Health-prescription-software-API/Data/Entities/Chat/ChatMessage.cs
@@ -11,15 +11,16 @@
         public Guid Id { get; set; }
 
         [Required]
-        [Length(minimumLength: 1, maximumLength: 500)]
+        [StringLength(500, MinimumLength = 1)]
         public string Message { get; set; } = null!;
 
+        [Column(TypeName = "Timestamp")]
         public DateTime MessageTime { get; set; }
 
         [Required]
-        [ForeignKey(nameof(SenderId))]
-        public string SenderId { get; set; } = null!;
-        public virtual User Sender { get; set; } = null!;
+        [ForeignKey(nameof(AuthorId))]
+        public string AuthorId { get; set; } = null!;
+        public virtual User Author { get; set; } = null!;
 
         [DefaultValue(false)]
         public bool IsRead { get; set; }
